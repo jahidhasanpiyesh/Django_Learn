04049e5c904ccf81f603267917770a3397ffa4cb
Md. Jahid Hasan <122719439+jahidhasanpiyesh@users.noreply.github.com>
2023-11-01 23:30:49 +0600

    Multiple Dict data store and access
    Multiple data store one variable and access to show output all data access to step by step .

---
+++ Code 18/Studentapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def student(request):
+    
+    # Multiple data use to dict and stor this {st} variable .
+    
+    st = {
+        'st1':{'name':'Jahid','roll':472826},
+        'st2':{'name':'saddam','roll':472772},
+        'st3':{'name':'naeem','roll':472792},
+        'st4':{'name':'mamun','roll':472795}
+    }
+    student_info = {'data':st}
+    return render(request, 'index.html',student_info) 
