11753beba442d70b74168d1d7448c0ae194aa46a
Md. Jahid Hasan <122719439+jahidhasanpiyesh@users.noreply.github.com>
2023-11-17 21:03:46 +0600

    How to use Bootstrap and Font Awesome in Django
    How to use Bootstrap css or js code in Django
    How to use Font Awesome code in Django

---
+++ Code 24/corinfo/static/corinfo/js/corinfo.js
@@ -0,0 +1,12 @@
+function home(){
+    alert("HI Ther I am HOME Page")
+}
+
+function about(){
+    alert("HI Ther I am About Page")
+}
+
+function course(){
+    alert("HI Ther I am Course Page")
+}
+
