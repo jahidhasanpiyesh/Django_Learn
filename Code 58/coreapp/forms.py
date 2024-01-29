c21d1350f0da0fda223ff76ad25681d9aff2295d
Md. Jahid Hasan <122719439+jahidhasanpiyesh@users.noreply.github.com>
2024-01-29 16:23:30 +0600

    User Profile and Admin Profile
    User Profile and Admin Profile  show

---
+++ Code 58/coreapp/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+
+class UserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+        
+class UserChange(UserChangeForm):
+    password = None
+    class Meta:
+        model =User
+        fields = ['username', 'first_name', 'last_name', 'email','last_login','date_joined']
+        
+class UserAdminChange(UserChangeForm):
+    password = None
+    class Meta:
+        model =User
+        fields = '__all__'
---
+++ Code 58/coreapp/views.py
@@ -0,0 +1,111 @@
+from django.shortcuts import render, HttpResponseRedirect
+from .forms import UserForm, UserChange, UserAdminChange
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm,PasswordChangeForm
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.models import User
+# Create your views here.
+
+
+# Singup Base Functions --------
+def Sign_up(request):
+    if request.method == 'POST':
+        fm = UserForm(request.POST)
+        if fm.is_valid():  # chack valid data 
+            messages.success(request, 'Your Form is Submit Successfully !!')
+            fm.save()
+               
+    else:
+        fm = UserForm()
+    return render(request, 'coreapp/signup.html', {'from':fm})
+
+
+# Login Base Functions ------
+def log_in(request):
+    
+    # please chack user data is_authenticated 
+    if not request.user.is_authenticated:
+        if request.method == 'POST':   # use to Post method 
+            # import AuthenticationForm()  in ---from django.contrib.auth.forms
+            fm = AuthenticationForm(request=request, data=request.POST)
+            if fm.is_valid():    # chack valid data
+                # and call cleaned_data 
+                uname = fm.cleaned_data['username']
+                upass = fm.cleaned_data['password']
+                user = authenticate(username=uname, password=upass)
+                if user is not None:
+                     # import login() in ---from django.contrib.auth
+                    login(request,user)
+                    # built message funciton just user -- 
+                    messages.success(request, 'Your Account Login Successfully !!')
+                    # import HttpResponseRedirect()
+                    return HttpResponseRedirect('/profile/')   
+        else:
+            fm = AuthenticationForm()  
+        return render(request, 'coreapp/login.html', {'from':fm})
+    else:
+        return HttpResponseRedirect('/profile/')
+    
+    
+# Logout Base Functions -----
+def log_out(request):
+    # import logout() in ---from django.contrib.auth
+    logout(request)
+    return HttpResponseRedirect('/login/')
+
+
+# Profile Base Functions -----
+def profile(request):
+    # Please Call is_authenticated Functions 
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if request.user.is_superuser == True:
+                fm = UserAdminChange(request.POST, instance=request.user)
+                users = User.objects.all()
+            else:
+                # inharetance forms.py class 
+                fm = UserChange(request.POST, instance = request.user)
+                users = None
+            if fm.is_valid():
+                messages.success(request,'Your Profile Data Update Successfully !!')
+                fm.save()
+        else:
+            if request.user.is_superuser == True :
+                fm = UserAdminChange(instance=request.user)
+                users = User.objects.all()
+            else:
+                fm = UserChange(instance=request.user)
+                users = None
+        return render(request,'coreapp/profile.html',{'name':request.user.username,'form':fm, 'users':users})
+    else:
+        return HttpResponseRedirect('/login/')
+
+# Change Password Without old password just call setpasswordform()--functions with pass same code --
+# Change Password With old password Functions Base------
+def changepass(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            # please import --PasswordChangeForm() in --from django.contrib.auth.forms
+            fm = PasswordChangeForm(user=request.user, data=request.POST)
+            if fm.is_valid():
+                fm.save()
+                
+                # session maintain i mean its call then run and solve this 
+                # submit change pass and show profile page  
+                update_session_auth_hash(request,fm.user)
+                messages.success(request, 'Your Password Change Successfully !!')
+                return HttpResponseRedirect('/profile/')
+        else:
+            fm = PasswordChangeForm(user=request.user)
+        return render(request,'coreapp/changepass.html',{'form':fm})
+    
+    else:
+        return HttpResponseRedirect('/login/')
+    
+def userinfo(request, id):
+    if request.user.is_authenticated:
+        pi = User.objects.get(pk=id)
+        fm = UserAdminChange(instance=pi)
+        return render(request,'coreapp/userinfo.html',{'form':fm})
+    else:
+        return HttpResponseRedirect('/login/')
